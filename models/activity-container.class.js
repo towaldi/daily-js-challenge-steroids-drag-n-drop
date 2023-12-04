e3f8b32b1c91057dd05dd1ef4a4b757e26383cfe
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-12-04 20:18:11 +0100

    add instance activity into array + display

--- models/activity-container.class.js
+++ models/activity-container.class.js
@@ -1,15 +1,10 @@
 class Activity {
-    constructor(header, step) {
-        this._header = header,
-        this._step = step
+    constructor(header) {
+        this._header = header
     }
 
     // getters
     get header() {
         return this._header;
-    }
-
-    get step() {
-        return this._step;
     }
 }
--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-let activities = [];
+// Arrays
+const activities = [];
 
-// btns
-const addActivityBtn = document.getElementById('add-activity-btn');
-// dialogs/modal
-const modalAddActivity = document.getElementById('dialog-add-activity');
-const newActivityHeader = document.getElementById('new-activity-header');
-const closeBtn = document.querySelector('.close-btn');
-const confirmBtn = document.querySelector('.confirm-btn');
+// Elements
+// Buttons
+const openAddActivityDialogBtn = document.getElementById('open-add-activity-btn');
+const closeAddActivityDialogBtn = document.getElementById('close-add-activity-btn');
+const confirmAddActivityDialogBtn = document.getElementById('confirm-add-activity-btn');
+// Dialogs
+const addActivityDialog = document.getElementById('add-activity-dialog');
+const inputActivityName = document.getElementById('input-new-activity');
 
 
-// 'addActivityBtn' button opens the <dialog> modally
-addActivityBtn.addEventListener('click', () => {
-    modalAddActivity.showModal();
+// Opens "add activity" dialog
+openAddActivityDialogBtn.addEventListener('click', () => {
+    addActivityDialog.showModal();
+});
+
+// Closes "add activity" dialog
+closeAddActivityDialogBtn.addEventListener('click', () => {
+    addActivityDialog.close();
 });
 
 
-//
-closeBtn.addEventListener('click', () => {
-    modalAddActivity.close();
+// Confirm "add activity"
+confirmAddActivityDialogBtn.addEventListener('click', () => {
+    // Assigns value from input to 'activityName'
+    const activityName = inputActivityName.value;
+    // Checks if input is not empty
+    if (activityName.trim() !== '') {
+        // Create instance of activity -> assign to 'newActivity'
+        const newActivity = new Activity(activityName);
+        // Push it into array
+        activities.push(newActivity);
+        // Display activities in container
+        displayActivities();
+        // Closes "add activity" dialog
+        addActivityDialog.close();
+        // Clears input field
+        inputActivityName.value = '';
+    } else {
+        // Informs user that input is empty
+        alert('Please enter a valid activity name!');
+    }
 });
 
+function displayActivities() {
+    const content = document.getElementById('content');
+    // Clear the previous content
+    content.innerHTML = '';
+    // Display each activity in the array
+    activities.forEach(activity => {
+        content.innerHTML += /*html*/    `<div class="activity-container">
+                                            <div class="a-c-header">
+                                                <h3>${activity.header}</h3>
+                                            </div>
+                                            <div class="a-c-content">
+                                                <!-- content: steps + details -->
+                                            </div>
+                                        </div>`;
+    });
 
-
-/**
- * class activity
- */
-
-class Activity {
-    constructor(header, step) {
-        this._header = header,
-        this._step = step
-    }
-
-    // getters
-    get header() {
-        return this._header;
-    }
-
-    get step() {
-        return this._step;
-    }
-}
-
-// function addActivity() {
-    
-// }
+};
